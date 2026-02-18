ede875094863860cea6cd302bd605c392f9916a9
Jori <jori@MacBook-Pro-cua-Jori.local>
2026-02-18 12:44:51 +0700

    update TON manifest

--- components/TonConnectProvider.tsx
+++ components/TonConnectProvider.tsx
@@ -3,7 +3,9 @@
 import { TonConnectUIProvider } from '@tonconnect/ui-react';
 import { ReactNode } from 'react';
 
-const manifestUrl = 'https://raw.githubusercontent.com/ton-community/tutorials/main/03-client/test/public/tonconnect-manifest.json';
+const manifestUrl = typeof window !== 'undefined' 
+  ? `${window.location.origin}/tonconnect-manifest.json`
+  : '/tonconnect-manifest.json';
 
 export function TonConnectProvider({ children }: { children: ReactNode }) {
   return (
